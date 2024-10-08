2b1dfe82e66def3aefbd240ecbee1d18a11eff04
Aaron Becerril Sanchez <noraasehcnas@gmail.com>
2024-10-08 19:13:01 +0200

    start project

---
+++ MereDansk/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MereDansk
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MereDansk/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MereDansk
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
